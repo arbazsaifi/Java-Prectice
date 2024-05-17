e557bf4cd9f6cfaf6505fd7336be9c768b6e0b29
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-05-17 19:40:33 +0530

    class and objects

---
+++ 17.05.24/Main.java
@@ -0,0 +1,24 @@
+// OOP (Java)
+/*------------------------------------------------------------------------------------*/ 
+/*-------------------------------- Class and Object ----------------------------------*/ 
+
+// class Car {
+//     String color;
+//     String model;
+
+//     void drive() {
+//         System.out.println("Model: " + this.model + " and Color: " + this.color);
+//     }
+// }
+
+// class Main {
+//     public static void main(String[] args) {
+//         Car myCar = new Car();
+        
+//         myCar.color = "Red";
+//         myCar.model = "G-Wagon";
+
+//         myCar.drive();
+//     }
+// }
+
