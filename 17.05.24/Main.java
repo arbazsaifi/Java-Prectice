926b6f325e521432d151e96bb9b708cfcf859c3e
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-05-17 19:40:53 +0530

    inheritance

--- 17.05.24/Main.java
+++ 17.05.24/Main.java
@@ -22,3 +22,28 @@
 //     }
 // }
 
+
+/*-------------------------------------------------------------------------------*/ 
+/*-------------------------------- Inheritance ----------------------------------*/
+// class Animal {
+//     void eat() {
+//         System.out.println("Animal eats food");
+//     }
+// }
+
+// // Inherit class from Animal class
+// class Dog extends Animal {
+//     void bark() {
+//         System.out.println("The dog is barking");
+//     }
+// }
+
+// class Main {
+//     public static void main(String[] args) {
+//         Dog myDog = new Dog();
+
+//         myDog.eat();   //method from Animal class
+//         myDog.bark();  //method from Dog class
+//     }
+// }
+
