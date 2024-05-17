fedc9a0c4b8083c640027404922c9e28b72bbfec
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2024-05-17 19:42:25 +0530

    constructor overloading

--- 17.05.24/Main.java
+++ 17.05.24/Main.java
@@ -116,3 +116,58 @@
 //     }
 // }
 
+/*----------------------------------------------------------------------------------------*/
+/*-------------------------------- Constructor Overloading ----------------------------------*/
+/*** class having multiple constructor with different number of parameters ***/
+// class Main {
+//     public static void main(String[] args) {
+//         person per1 = new person();
+//         person per2 = new person("Arbaz");
+//         person per3 = new person("Amir", 22);
+//         person per4 = new person("Raju", 24, "New Delhi");
+
+//         per1.personDetail();
+//         per2.personDetail();
+//         per3.personDetail();
+//         per4.personDetail();
+//     }
+// }
+
+// class person {
+//     String name;
+//     int age;
+//     String city;
+
+//     person() {
+//         this.name = "unknown";
+//         this.age = 0;
+//         this.city = "unknown";
+//     }
+
+//     person(String name) {
+//         this.name = name;
+//         this.age = 0;
+//         this.city = "unknown";
+//     }
+
+//     person(String name, int age) {
+//         this.name = name;
+//         this.age = age;
+//         this.city = "unknown";
+//     }
+
+//     person(String name, int age, String city) {
+//         this.name = name;
+//         this.age = age;
+//         this.city = city;
+//     }
+
+//     void personDetail() {
+//         System.out.println("Name: " + this.name);
+//         System.out.println("Age: " + this.age);
+//         System.out.println("City: " + this.city);
+//         System.out.println("");
+//     }
+
+// }
+
